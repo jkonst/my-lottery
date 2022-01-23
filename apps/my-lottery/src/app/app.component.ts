39d9df3b904053d7ec1a5398d7ed1a159c641118
jkonst <yannis.konstas@gmail.com>
2022-01-23 22:13:29 +0200

    initial commit

---
+++ apps/my-lottery/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jkonst-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'my-lottery';
+}
